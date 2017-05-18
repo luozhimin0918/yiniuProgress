aadf0c8a907281f2c0878fe95b8dd85d6a0bdc11
代瑶 <991275257@qq.com>
2017-05-18 11:44:43 +0800

    增加OnRequest名字

---
+++ app/src/main/java/com/jyh/kxt/base/impl/OnRequestPermissions.java
@@ -0,0 +1,17 @@
+package com.jyh.kxt.base.impl;
+
+/**
+ * Created by Mr'Dai on 2017/5/16.
+ */
+
+public interface OnRequestPermissions {
+    /**
+     * 调用成功
+     */
+    void doSomething();
+
+    /**
+     * 权限失败
+     */
+    void doFailSomething();
+}
