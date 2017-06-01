1eb78d3db4a66b2c97d66c998b1f7116bc9c74e9
苟蒙蒙 <465532252@qq.com>
2017-06-01 17:52:41 +0800

    New changelist

---
+++ publiclibrary/build/generated/source/dataBinding/androidTest/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="1c0b47f7-beb1-465d-9a15-a94e1aa1de6f", modulePackage="com.library.test", sdkRoot="D:\\sdk\\sdk",layoutInfoDir="D:\\git\\kxt\\Kxt\\publiclibrary\\build\\intermediates\\data-binding-info\\androidTest\\debug",exportClassListTo="",isLibrary=false,minSdk=14,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
