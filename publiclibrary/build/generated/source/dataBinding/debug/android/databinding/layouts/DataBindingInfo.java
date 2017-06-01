a176dfb739788a05164a79b30efcf380d13e6828
苟蒙蒙 <465532252@qq.com>
2017-06-01 17:57:05 +0800

    New changelist

---
+++ publiclibrary/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="108648dd-d0ce-4373-9ee5-899cc0d04cbe", modulePackage="com.library", sdkRoot="D:\\sdk\\sdk",layoutInfoDir="D:\\git\\kxt\\Kxt\\publiclibrary\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="D:\\git\\kxt\\Kxt\\publiclibrary\\build\\intermediates\\data-binding-info\\debug\\_generated.txt",isLibrary=true,minSdk=14,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
---
+++ publiclibrary/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="98442e13-8e73-4423-8b81-395bd12692ae", modulePackage="com.library", sdkRoot="D:\\sdk\\sdk",layoutInfoDir="D:\\git\\kxt\\Kxt\\publiclibrary\\build\\intermediates\\data-binding-info\\release",exportClassListTo="D:\\git\\kxt\\Kxt\\publiclibrary\\build\\intermediates\\data-binding-info\\release\\_generated.txt",isLibrary=true,minSdk=14,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
