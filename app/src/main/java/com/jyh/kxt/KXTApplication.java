f1e59db0cf9dd3577c1072bdc8e75afdc8ace5cc
代瑶 <991275257@qq.com>
2017-06-13 17:57:37 +0800

    增加Tinker 热修复

--- app/src/main/java/com/jyh/kxt/KXTApplication.java
+++ app/src/main/java/com/jyh/kxt/KXTApplication.java
@@ -1,75 +1,75 @@
-package com.jyh.kxt;
-
-import android.app.Application;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Handler;
-import android.support.multidex.MultiDex;
-
-import com.jyh.kxt.base.utils.UmengShareTool;
-import com.jyh.kxt.index.service.PreLoadX5Service;
-import com.jyh.kxt.push.KXTPushIntentService;
-import com.umeng.message.IUmengRegisterCallback;
-import com.umeng.message.MsgConstant;
-import com.umeng.message.PushAgent;
-import com.umeng.message.common.UmLog;
-
-/**
- * 项目名:Kxt
- * 类描述:
- * 创建人:苟蒙蒙
- * 创建日期:2017/4/12.
- */
-
-public class KXTApplication extends Application {
-
-    private String TAG = "KXTApplication";
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        //初始化umeng分享
-        UmengShareTool.initUmeng(this);
-        initPush();
-    }
-
-    private void initPush() {
-
-        final Handler handler = new Handler();
-        PushAgent mPushAgent = PushAgent.getInstance(this);
-        //sdk开启通知声音
-        //mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
-        // sdk关闭通知声音
-        //mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_DISABLE);
-        //通知声音由服务端控制
-        mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SERVER);
-
-        //注册推送服务 每次调用register都会回调该接口
-        mPushAgent.register(new IUmengRegisterCallback() {
-            @Override
-            public void onSuccess(String deviceToken) {
-                UmLog.i(TAG, "device token: " + deviceToken);
-            }
-
-            @Override
-            public void onFailure(String s, String s1) {
-                UmLog.i(TAG, "register failed: " + s + " " + s1);
-            }
-        });
-
-//        CrashHandler crashHandler = new CrashHandler();
-//        crashHandler.init(this);
-
-        mPushAgent.setPushIntentServiceClass(KXTPushIntentService.class);
-
-        //避免启动慢的问题
-        Intent intent = new Intent(this, PreLoadX5Service.class);
-        this.startService(intent);
-    }
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
-    }
-}
+//package com.jyh.kxt;
+//
+//import android.app.Application;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.os.Handler;
+//import android.support.multidex.MultiDex;
+//
+//import com.jyh.kxt.base.utils.UmengShareTool;
+//import com.jyh.kxt.index.service.PreLoadX5Service;
+//import com.jyh.kxt.push.KXTPushIntentService;
+//import com.umeng.message.IUmengRegisterCallback;
+//import com.umeng.message.MsgConstant;
+//import com.umeng.message.PushAgent;
+//import com.umeng.message.common.UmLog;
+//
+///**
+// * 项目名:Kxt
+// * 类描述:
+// * 创建人:苟蒙蒙
+// * 创建日期:2017/4/12.
+// */
+//
+//public class KXTApplication extends Application {
+//
+//    private String TAG = "KXTApplication";
+//
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//        //初始化umeng分享
+//        UmengShareTool.initUmeng(this);
+//        initPush();
+//    }
+//
+//    private void initPush() {
+//
+//        final Handler handler = new Handler();
+//        PushAgent mPushAgent = PushAgent.getInstance(this);
+//        //sdk开启通知声音
+//        //mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
+//        // sdk关闭通知声音
+//        //mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_DISABLE);
+//        //通知声音由服务端控制
+//        mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SERVER);
+//
+//        //注册推送服务 每次调用register都会回调该接口
+//        mPushAgent.register(new IUmengRegisterCallback() {
+//            @Override
+//            public void onSuccess(String deviceToken) {
+//                UmLog.i(TAG, "device token: " + deviceToken);
+//            }
+//
+//            @Override
+//            public void onFailure(String s, String s1) {
+//                UmLog.i(TAG, "register failed: " + s + " " + s1);
+//            }
+//        });
+//
+////        CrashHandler crashHandler = new CrashHandler();
+////        crashHandler.init(this);
+//
+//        mPushAgent.setPushIntentServiceClass(KXTPushIntentService.class);
+//
+//        //避免启动慢的问题
+//        Intent intent = new Intent(this, PreLoadX5Service.class);
+//        this.startService(intent);
+//    }
+//
+//    @Override
+//    protected void attachBaseContext(Context base) {
+//        super.attachBaseContext(base);
+//        MultiDex.install(this);
+//    }
+//}
---
+++ app/src/main/java/com/jyh/kxt/base/tinker/app/SampleApplicationLike.java
@@ -0,0 +1,155 @@
+/*
+ * Tencent is pleased to support the open source community by making Tinker available.
+ *
+ * Copyright (C) 2016 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jyh.kxt.base.tinker.app;
+
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.support.multidex.MultiDex;
+
+import com.jyh.kxt.base.tinker.Log.MyLogImp;
+import com.jyh.kxt.base.tinker.util.SampleApplicationContext;
+import com.jyh.kxt.base.tinker.util.TinkerManager;
+import com.jyh.kxt.base.utils.UmengShareTool;
+import com.jyh.kxt.index.service.PreLoadX5Service;
+import com.jyh.kxt.push.KXTPushIntentService;
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.app.ApplicationLifeCycle;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.MsgConstant;
+import com.umeng.message.PushAgent;
+import com.umeng.message.common.UmLog;
+
+
+/**
+ * because you can not use any other class in your application, we need to
+ * move your implement of Application to {@link ApplicationLifeCycle}
+ * As Application, all its direct reference class should be in the main dex.
+ * <p>
+ * We use tinker-android-anno to make sure all your classes can be patched.
+ * <p>
+ * application: if it is start with '.', we will add SampleApplicationLifeCycle's package name
+ * <p>
+ * flags:
+ * TINKER_ENABLE_ALL: support dex, lib and resource
+ * TINKER_DEX_MASK: just support dex
+ * TINKER_NATIVE_LIBRARY_MASK: just support lib
+ * TINKER_RESOURCE_MASK: just support resource
+ * <p>
+ * loaderClass: define the tinker loader class, we can just use the default TinkerLoader
+ * <p>
+ * loadVerifyFlag: whether check files' md5 on the load time, defualt it is false.
+ * <p>
+ * Created by zhangshaowen on 16/3/17.
+ */
+@SuppressWarnings("unused")
+@DefaultLifeCycle(application = "com.jyh.kxt.KXTApplication",
+                  flags = ShareConstants.TINKER_ENABLE_ALL,
+                  loadVerifyFlag = false)
+public class SampleApplicationLike extends DefaultApplicationLike {
+    private static final String TAG = "Tinker.SampleApplicationLike";
+
+    public SampleApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag,
+                                 long applicationStartElapsedTime, long applicationStartMillisTime, Intent
+                                         tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime,
+                applicationStartMillisTime, tinkerResultIntent);
+    }
+
+    /**
+     * install multiDex before install tinker
+     * so we don't need to put the tinker lib classes in the main dex
+     *
+     * @param base
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        //you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+
+        SampleApplicationContext.application = getApplication();
+        SampleApplicationContext.context = getApplication();
+        TinkerManager.setTinkerApplicationLike(this);
+
+        TinkerManager.initFastCrashProtect();
+        //should set before tinker is installed
+        TinkerManager.setUpgradeRetryEnable(true);
+
+        //optional set logIml, or you can use default debug log
+        TinkerInstaller.setLogIml(new MyLogImp());
+
+        //installTinker after load multiDex
+        //or you can put com.tencent.tinker.** to main dex
+        TinkerManager.installTinker(this);
+        Tinker tinker = Tinker.with(getApplication());
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initApplication();
+    }
+
+    private void initApplication() {
+        UmengShareTool.initUmeng(SampleApplicationContext.context);
+        final Handler handler = new Handler();
+        PushAgent mPushAgent = PushAgent.getInstance(SampleApplicationContext.context);
+
+        //sdk开启通知声音
+        //mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
+        // sdk关闭通知声音
+        //mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_DISABLE);
+        //通知声音由服务端控制
+        mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SERVER);
+
+        //注册推送服务 每次调用register都会回调该接口
+        mPushAgent.register(new IUmengRegisterCallback() {
+            @Override
+            public void onSuccess(String deviceToken) {
+                UmLog.i(TAG, "device token: " + deviceToken);
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+                UmLog.i(TAG, "register failed: " + s + " " + s1);
+            }
+        });
+
+//        CrashHandler crashHandler = new CrashHandler();
+//        crashHandler.init(this);
+        mPushAgent.setPushIntentServiceClass(KXTPushIntentService.class);
+        //避免启动慢的问题
+        Intent intent = new Intent(SampleApplicationContext.context, PreLoadX5Service.class);
+        SampleApplicationContext.context.startService(intent);
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
+    }
+
+}
