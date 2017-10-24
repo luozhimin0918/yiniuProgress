bc52abd2dc27b74f643d40784c282a372dea0420
代瑶 <991275257@qq.com>
2017-10-24 17:13:27 +0800

    增加忘记密码

---
+++ app/src/main/java/com/jyh/kxt/user/ui/AlterPassActivity.java
@@ -0,0 +1,20 @@
+package com.jyh.kxt.user.ui;
+
+import android.os.Bundle;
+
+import com.jyh.kxt.R;
+import com.jyh.kxt.base.BaseActivity;
+
+/**
+ * 忘记密码
+ * Mr'Dai
+ */
+public class AlterPassActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alter_pass, StatusBarColor.THEME1);
+
+    }
+}
